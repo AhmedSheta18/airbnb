8b329fb328f54b9f960b5b707de53a5c3849fc45
Ahmed Sheta <Ahmed.Shetaa.2003@gmail.com>
2025-07-29 12:06:04 +0300

    ### **Lecture6.1** add accounts app from anither app

--- project/urls.py
+++ project/urls.py
@@ -20,8 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('settings.urls', namespace='home')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('admin/', admin.site.urls),
     path('property/', include('property.urls', namespace='property')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('about/', include('about.urls', namespace="about")), 
