757fd1c02f75c66ebb0e24056dfd227be0954fab
Ahmed Sheta <Ahmed.Shetaa.2003@gmail.com>
2025-08-04 14:49:48 +0300

    ### **Lecture7** Refactor URL patterns to include booking details and properties views and  create my booking

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profile, profile_edit, signup #, my_reservation , add_feedback
+from .views import profile, profile_edit, signup, my_reservation, add_feedback, my_properties, booking_details
 from accounts.views import custom_logout
 
 
@@ -7,10 +7,15 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    #this profile and edit profile are for the user
     path('profile/', profile, name='profile'),
     path('profile/edit', profile_edit, name='profile_edit'),
-    path('logout/', custom_logout, name='logout'),  # Custom logout view
-
-    # path('profile/booking', my_reservation , name='my_reservation') ,
-    # path('profile/booking/<slug:slug>/review', add_feedback , name='add_feedback') 
+    # this is ia my booking page and booking details and review
+    path('profile/booking', my_reservation, name='my_reservation'),
+    path('profile/booking/<int:booking_id>/', booking_details, name='booking_details'),
+    path('profile/booking/<slug:slug>/review', add_feedback, name='add_feedback'),
+    # this is for the user to see their properties
+    path('profile/properties', my_properties, name='my_properties'),
+    # Custom logout view
+    path('logout/', custom_logout, name='logout'),  
 ]
